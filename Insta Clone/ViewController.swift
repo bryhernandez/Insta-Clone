db56325252026a5e5560b3902b60170c171b5e48
bryanhdz <bryannhdez@gmail.com>
2021-10-03 08:09:54 -0700

    Initial Commit

---
+++ Insta Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta Clone
+//
+//  Created by Bryan Hernandez on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
